9f306b36e29cf13e43f4720e8b18ed5d1fb0814f
Lena E <elena.evdokimova@softwire.com>
2021-06-29 14:33:52 +0100

    recieved data

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+// foreach(string line in lines)
+// {
+//     string[] columns = line.Split(',');
+//     foreach (string column in columns) {
+//         // Do something
+//     }
+// }
+
+namespace Support_Bank_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "./Data/Transactions2014.csv";
+
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',');
+                Console.WriteLine(line);
+                foreach (string column in columns)
+                {
+                    // Console.Write(column);
+                }   
+            }
+        }
+    }
+}
